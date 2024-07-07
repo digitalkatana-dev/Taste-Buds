f561e4c260deac2f54ed88c8eb6c342683054f37
nucleusdev01 <dev@nucleusinsurance.com>
2024-07-07 12:29:46 -0700

    cleaned up some styling

--- taste-buds-web/src/pages/Matches/Matches.jsx
+++ taste-buds-web/src/pages/Matches/Matches.jsx
@@ -1,11 +1,14 @@
 import { Container } from '@mui/material';
 import './matches.scss';
+import MatchDisplay from '../../components/MatchDisplay';
 import BottomNav from '../../components/BottomNav';
 
 const Matches = () => {
 	return (
 		<Container id='matches' maxWidth='lg'>
-			<div className='content-container'></div>
+			<div className='content-container'>
+				<MatchDisplay />
+			</div>
 			<BottomNav />
 		</Container>
 	);
