0ce72f5530f3383b477fcfda72f3e1ca41c26fe2
nucleusdev01 <dev@nucleusinsurance.com>
2024-08-13 16:02:15 -0700

    moved user profile section form SelectedProfile and Dashboard to new UserProfile component

--- taste-buds-web/src/pages/Dashboard/Dashboard.jsx
+++ taste-buds-web/src/pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,3 @@
-import { Chip, Divider, Paper, Stack } from '@mui/material';
 import {
 	useCallback,
 	useEffect,
@@ -8,23 +7,17 @@ import {
 	createRef,
 } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-	setWarningType,
-	setWarningOpen,
-	setSelectedProfile,
-} from '../../redux/slices/appSlice';
+import { setSelectedProfile } from '../../redux/slices/appSlice';
 import {
 	getGenderedBuds,
 	updateMatches,
-	updateBlocked,
 	clearSuccess,
 } from '../../redux/slices/userSlice';
-import { capitalizeFirstLetterOfEachWord } from '../../util/helpers';
 import TinderCard from 'react-tinder-card';
 import './dashboard.scss';
 import SideBar from '../../components/SideBar';
+import UserProfile from '../../components/UserProfile';
 import ButtonRow from '../../components/ButtonRow';
-import Button from '../../components/Button';
 
 const Dashboard = () => {
 	const { isMobile, selectedProfile } = useSelector((state) => state.app);
@@ -40,8 +33,6 @@ const Dashboard = () => {
 		[allUsers]
 	);
 	const dispatch = useDispatch();
-	const theme = activeUser?.theme;
-	const blocked = activeUser?.blocked;
 
 	const updateCurrentIndex = (val) => {
 		setCurrentIndex(val);
@@ -51,11 +42,6 @@ const Dashboard = () => {
 	const canGoBack = currentIndex < allUsers?.length - 1;
 
 	const canSwipe = currentIndex >= 0;
-
-	const blockedCheck = () => {
-		return blocked.some((item) => item === selectedProfile?._id);
-	};
-	const isBlocked = blockedCheck();
 
 	const swiped = (direction, swippedProfileId, index) => {
 		const matches = activeUser?.matches;
@@ -99,45 +85,6 @@ const Dashboard = () => {
 		setLastDirection(undefined);
 	};
 
-	const handleBlock = () => {
-		if (isBlocked) {
-			const unblocked = blocked?.filter(
-				(item) => item !== selectedProfile?._id
-			);
-			const data = {
-				profileId: activeUser._id,
-				blocked: unblocked,
-			};
-
-			dispatch(updateBlocked(data));
-		} else {
-			dispatch(setWarningType('block'));
-			dispatch(setWarningOpen(true));
-		}
-	};
-
-	const handleUnmatch = () => {
-		const matches = activeUser?.matches;
-		const matchCheck = () => {
-			return matches.some((match) => match._id === selectedProfile._id);
-		};
-		const areMatched = matchCheck();
-
-		if (areMatched) {
-			const updatedMatches = matches.filter(
-				(item) => item._id !== selectedProfile?._id
-			);
-			const data = {
-				profileId: activeUser._id,
-				matches: updatedMatches,
-			};
-
-			dispatch(updateMatches(data));
-		} else {
-			return;
-		}
-	};
-
 	const handleGetUsers = useCallback(() => {
 		dispatch(getGenderedBuds(activeUser?.genderInterest));
 	}, [dispatch, activeUser]);
@@ -168,140 +115,7 @@ const Dashboard = () => {
 		<div id='dashboard'>
 			<SideBar />
 			{!isMobile && selectedProfile ? (
-				<div className='selected-profile'>
-					<div className='header-container'>
-						<div className='cover-photo-section'>
-							<div className='cover-photo-container'>
-								{selectedProfile?.coverPhoto && (
-									<img src={selectedProfile?.coverPhoto} alt='cover' />
-								)}
-							</div>
-						</div>
-					</div>
-					<section id='profile-content'>
-						<Stack
-							direction='column'
-							justifyContent='center'
-							alignItems='center'
-							gap={1}
-							className='user-container'
-						>
-							<div className='profile-photo-container'>
-								<img
-									src={selectedProfile?.profilePhoto}
-									alt={selectedProfile?.handle}
-								/>
-							</div>
-							<Stack direction='column' alignItems='center'>
-								<span className='full-name'>
-									{selectedProfile?.firstName + ' ' + selectedProfile?.lastName}
-								</span>
-								<span className='handle'>@{selectedProfile?.handle}</span>
-							</Stack>
-						</Stack>
-						<Paper
-							className={
-								theme === 'dark' ? 'profile-paper dark' : 'profile-paper'
-							}
-							elevation={10}
-						>
-							<Divider>
-								<Chip label='About' size='small' className='divider-chip' />
-							</Divider>
-							<div className='profile-data-container'>
-								{selectedProfile?.about}
-							</div>
-							<Divider>
-								<Chip label='Diet' size='small' className='divider-chip' />
-							</Divider>
-							<div className='profile-data-container'>
-								<Chip
-									label={capitalizeFirstLetterOfEachWord(
-										selectedProfile?.dietType
-									)}
-									size='small'
-									variant={theme === 'dark' ? 'outlined' : 'filled'}
-									className='data-chip'
-								/>
-							</div>
-							<Divider>
-								<Chip
-									label='Food Types'
-									size='small'
-									className='divider-chip'
-								/>
-							</Divider>
-							<div className='profile-data-container'>
-								{selectedProfile?.favorites?.foodTypes?.map((item, i) => (
-									<Chip
-										key={i}
-										label={item}
-										size='small'
-										variant={theme === 'dark' ? 'outlined' : 'filled'}
-										className='data-chip'
-									/>
-								))}
-							</div>
-							<Divider>
-								<Chip label='Fav Dish' size='small' className='divider-chip' />
-							</Divider>
-							<div className='profile-data-container'>
-								<Chip
-									label={selectedProfile?.favorites?.dish}
-									size='small'
-									variant={theme === 'dark' ? 'outlined' : 'filled'}
-									className='data-chip'
-								/>
-							</div>
-							<Divider>
-								<Chip label='Food Porn' size='small' className='divider-chip' />
-							</Divider>
-							<div className='fp-photo-container'>
-								{selectedProfile?.images?.map((image, i) => (
-									<div className='fp-img' key={i}>
-										<img src={image} alt='' />
-									</div>
-								))}
-							</div>
-							<Divider>
-								<Chip
-									label='Block User'
-									size='small'
-									className='divider-chip'
-								/>
-							</Divider>
-							<div className='profile-data-container profile-action'>
-								<Button
-									size='small'
-									className={
-										isBlocked
-											? 'profile-action-btn unblock'
-											: 'profile-action-btn'
-									}
-									onClick={handleBlock}
-								>
-									{isBlocked ? 'Unblock' : 'Block'}
-								</Button>
-							</div>
-							<Divider>
-								<Chip
-									label='Remove Match'
-									size='small'
-									className='divider-chip'
-								/>
-							</Divider>
-							<div className='profile-data-container profile-action'>
-								<Button
-									size='small'
-									className='profile-action-btn'
-									onClick={handleUnmatch}
-								>
-									Unmatch
-								</Button>
-							</div>
-						</Paper>
-					</section>
-				</div>
+				<UserProfile />
 			) : (
 				<div className='swipe-container'>
 					<div className='card-container'>
--- taste-buds-web/src/pages/SelectedProfile/SelectedProfile.jsx
+++ taste-buds-web/src/pages/SelectedProfile/SelectedProfile.jsx
@@ -1,77 +1,22 @@
-import { Chip, Divider, Paper, Stack } from '@mui/material';
 import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 import {
 	getSelectedProfile,
 	setSelectedProfile,
-	setWarningOpen,
-	setWarningType,
 } from '../../redux/slices/appSlice';
-import {
-	updateMatches,
-	updateBlocked,
-	clearSuccess,
-} from '../../redux/slices/userSlice';
-import { capitalizeFirstLetterOfEachWord } from '../../util/helpers';
-import './selected.scss';
+import { clearSuccess } from '../../redux/slices/userSlice';
 import Loading from '../../components/Loading';
-import Button from '../../components/Button';
+import UserProfile from '../../components/UserProfile';
 
 const SelectedProfile = () => {
-	const { loading, selectedProfile } = useSelector((state) => state.app);
-	const { activeUser, success } = useSelector((state) => state.user);
+	const { loading } = useSelector((state) => state.app);
+	const { success } = useSelector((state) => state.user);
 	const location = useLocation();
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 
 	const profile = location.pathname.split('/')[2];
-	const theme = activeUser?.theme;
-	const blocked = activeUser?.blocked;
-
-	const blockedCheck = () => {
-		return blocked.some((item) => item === selectedProfile?._id);
-	};
-	const isBlocked = blockedCheck();
-
-	const handleUnmatch = () => {
-		const matches = activeUser?.matches;
-		const matchCheck = () => {
-			return matches.some((match) => match._id === selectedProfile._id);
-		};
-		const areMatched = matchCheck();
-
-		if (areMatched) {
-			const updatedMatches = matches.filter(
-				(item) => item._id !== selectedProfile?._id
-			);
-			const data = {
-				profileId: activeUser._id,
-				matches: updatedMatches,
-			};
-
-			dispatch(updateMatches(data));
-		} else {
-			return;
-		}
-	};
-
-	const handleBlock = () => {
-		if (isBlocked) {
-			const unblocked = blocked?.filter(
-				(item) => item !== selectedProfile?._id
-			);
-			const data = {
-				profileId: activeUser._id,
-				blocked: unblocked,
-			};
-
-			dispatch(updateBlocked(data));
-		} else {
-			dispatch(setWarningType('block'));
-			dispatch(setWarningOpen(true));
-		}
-	};
 
 	const loadSelectedProfile = useCallback(() => {
 		dispatch(getSelectedProfile(profile));
@@ -108,118 +53,7 @@ const SelectedProfile = () => {
 	return (
 		<div id='selected-profile'>
 			{loading && <Loading />}
-			<div className='header-container'>
-				<div className='cover-photo-section'>
-					<div className='cover-photo-container'>
-						{selectedProfile?.coverPhoto && (
-							<img src={selectedProfile?.coverPhoto} alt='cover' />
-						)}
-					</div>
-				</div>
-			</div>
-			<section id='profile-content'>
-				<Stack
-					direction='column'
-					justifyContent='center'
-					alignItems='center'
-					gap={1}
-					className='user-container'
-				>
-					<div className='profile-photo-container'>
-						<img
-							src={selectedProfile?.profilePhoto}
-							alt={selectedProfile?.handle}
-						/>
-					</div>
-					<Stack direction='column' alignItems='center'>
-						<span className='full-name'>
-							{selectedProfile?.firstName + ' ' + selectedProfile?.lastName}
-						</span>
-						<span className='handle'>@{selectedProfile?.handle}</span>
-					</Stack>
-				</Stack>
-				<Paper
-					className={theme === 'dark' ? 'profile-paper dark' : 'profile-paper'}
-					elevation={10}
-				>
-					<Divider>
-						<Chip label='About' size='small' className='divider-chip' />
-					</Divider>
-					<div className='profile-data-container'>{selectedProfile?.about}</div>
-					<Divider>
-						<Chip label='Diet' size='small' className='divider-chip' />
-					</Divider>
-					<div className='profile-data-container'>
-						<Chip
-							label={capitalizeFirstLetterOfEachWord(selectedProfile?.dietType)}
-							size='small'
-							variant={theme === 'dark' ? 'outlined' : 'filled'}
-							className='data-chip'
-						/>
-					</div>
-					<Divider>
-						<Chip label='Food Types' size='small' className='divider-chip' />
-					</Divider>
-					<div className='profile-data-container'>
-						{selectedProfile?.favorites?.foodTypes?.map((item, i) => (
-							<Chip
-								key={i}
-								label={item}
-								size='small'
-								variant={theme === 'dark' ? 'outlined' : 'filled'}
-								className='data-chip'
-							/>
-						))}
-					</div>
-					<Divider>
-						<Chip label='Fav Dish' size='small' className='divider-chip' />
-					</Divider>
-					<div className='profile-data-container'>
-						<Chip
-							label={selectedProfile?.favorites?.dish}
-							size='small'
-							variant={theme === 'dark' ? 'outlined' : 'filled'}
-							className='data-chip'
-						/>
-					</div>
-					<Divider>
-						<Chip label='Food Porn' size='small' className='divider-chip' />
-					</Divider>
-					<div className='fp-photo-container'>
-						{selectedProfile?.images?.map((image, i) => (
-							<div className='fp-img' key={i}>
-								<img src={image} alt='' />
-							</div>
-						))}
-					</div>
-					<Divider>
-						<Chip label='Block User' size='small' className='divider-chip' />
-					</Divider>
-					<div className='profile-data-container profile-action'>
-						<Button
-							size='small'
-							className={
-								isBlocked ? 'profile-action-btn unblock' : 'profile-action-btn'
-							}
-							onClick={handleBlock}
-						>
-							{isBlocked ? 'Unblock' : 'Block'}
-						</Button>
-					</div>
-					<Divider>
-						<Chip label='Remove Match' size='small' className='divider-chip' />
-					</Divider>
-					<div className='profile-data-container profile-action'>
-						<Button
-							size='small'
-							className='profile-action-btn'
-							onClick={handleUnmatch}
-						>
-							Unmatch
-						</Button>
-					</div>
-				</Paper>
-			</section>
+			<UserProfile />
 		</div>
 	);
 };
