7512220a6d63b6ef3b547623eaf31d008a6dd889
Brandon Benoit <b.nutty@gmail.com>
2024-08-17 10:10:11 -0700

    updated front end socket for prod

--- taste-buds-web/src/util/socket.js
+++ taste-buds-web/src/util/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-export const socket = io.connect('http://localhost:3005');
+export const socket = io.connect('https://taste-buds-backend.onrender.com');
 
 socket.on('connected', () => {
 	console.log('You are connected!');
