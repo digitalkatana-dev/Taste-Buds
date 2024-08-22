109f7b7f3555b37a216a2648d241db37ce7d080e
Brandon Benoit <brandon@digitalkatana.dev>
2024-08-22 10:02:09 -0700

    moved typing notification bubble to ChatBubble component

--- taste-buds-web/src/components/ChatDisplay/ChatDisplay.jsx
+++ taste-buds-web/src/components/ChatDisplay/ChatDisplay.jsx
@@ -1,6 +1,4 @@
-import { Chip } from '@mui/material';
 import { useSelector } from 'react-redux';
-import { TypeAnimation } from 'react-type-animation';
 import './chatDisplay.scss';
 import ChatBubble from '../ChatBubble';
 
@@ -13,39 +11,7 @@ const ChatDisplay = () => {
 			{conversation?.map((item) => (
 				<ChatBubble key={item._id} data={item} />
 			))}
-			{typing && (
-				<Chip
-					sx={{
-						height: 'auto',
-						'& .MuiChip-label': {
-							display: 'block',
-							whiteSpace: 'normal',
-						},
-					}}
-					label={
-						<span>
-							Typing
-							<TypeAnimation
-								deletionSpeed={100}
-								cursor={false}
-								sequence={[
-									'.', // Types '.'
-									500, // Waits 500ms
-									'..', // Types another '.'
-									500, // Waits 500ms
-									'...', // Types another '.'
-									500, // Waits 500ms
-									'', // Deletes everything
-									500, // Waits 500ms before restarting
-								]}
-								wrapper='span'
-								repeat={Infinity}
-							/>
-						</span>
-					}
-					className='chat-bubble'
-				/>
-			)}
+			{typing && <ChatBubble typing />}
 		</div>
 	);
 };
